7d9434cb43f27182af9b5671c2c2082a0221de8f
Moonkey <moonkey48@icloud.com>
2023-03-07 15:03:06 +0900

    [SimpleNote] start SimpleNote

---
+++ SimpleNote/SimpleNote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleNote
+//
+//  Created by Seungui Moon on 2023/03/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
